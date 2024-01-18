809c9d41c2e604de831559f22928c388ba80834d
Michael Gregory <mr.m.gregory@outlook.com>
2024-01-18 12:30:08 +0000

    fix

--- script.js
+++ script.js
@@ -133,79 +133,79 @@ const petChooser = document.querySelector("#petChooser")
 
 
 
-class Pet {
-    constructor(name) {
-        this.name = name;
-        this.health = 100;
-        this.hunger = 100;
-        this.xp = 0;
-    }
+// class Pet {
+//     constructor(name) {
+//         this.name = name;
+//         this.health = 100;
+//         this.hunger = 100;
+//         this.xp = 0;
+//     }
 
-    drinks() {
-        this.health += 5;
-        return this;
-    }
+//     drinks() {
+//         this.health += 5;
+//         return this;
+//     }
 
-    eats() {
-        this.health += 5;
-        this.hunger += 10;
-        console.log(`${this.name}'s health is ${this.health}`);
-    }
+//     eats() {
+//         this.health += 5;
+//         this.hunger += 10;
+//         console.log(`${this.name}'s health is ${this.health}`);
+//     }
 
-    stats() {
-        return console.table({
-            name: this.name,
-            health: this.health,
-            hunger: this.hunger,
-        });
-    }
-}
+//     stats() {
+//         return console.table({
+//             name: this.name,
+//             health: this.health,
+//             hunger: this.hunger,
+//         });
+//     }
+// }
 
-class Bulbasaur extends Pet {
-    constructor(name) {
-        super(name);
-        this.bulbasaurSpecial = 100;
-    }
+// class Bulbasaur extends Pet {
+//     constructor(name) {
+//         super(name);
+//         this.bulbasaurSpecial = 100;
+//     }
 
-    grass() {
-        this.bulbasaurSpecial += 10;
-        this.hunger -= 5;
-    }
-}
+//     grass() {
+//         this.bulbasaurSpecial += 10;
+//         this.hunger -= 5;
+//     }
+// }
 
-class Charmander extends Pet {
-    constructor(name) {
-        super(name);
-        this.charmanderSpecial = 100;
-    }
+// class Charmander extends Pet {
+//     constructor(name) {
+//         super(name);
+//         this.charmanderSpecial = 100;
+//     }
 
-    fire() {
-        this.charmanderSpecial += 10;
-        this.hunger -= 5;
-    }
-}
+//     fire() {
+//         this.charmanderSpecial += 10;
+//         this.hunger -= 5;
+//     }
+// }
 
-class Squirtle extends Pet {
-    constructor(name) {
-        super(name);
-        this.squirtleSpecial = 100;
-    }
+// class Squirtle extends Pet {
+//     constructor(name) {
+//         super(name);
+//         this.squirtleSpecial = 100;
+//     }
 
-    water() {
-        this.squirtleSpecial += 10;
-        this.hunger -= 5;
-    }
-}
+//     water() {
+//         this.squirtleSpecial += 10;
+//         this.hunger -= 5;
+//     }
+// }
 
-const petChooser = document.getElementById("petChooser");
+// const petChooser = document.getElementById("petChooser");
 
-petChooser.addEventListener("click", (event) => {
-    const clickbutton = event.target;
-    if (clickbutton.matches("#bulbasaur")) {
+// petChooser.addEventListener("click", (event) => {
+//     const clickbutton = event.target;
+//     if (clickbutton.matches("#bulbasaur")) {
       
-    } else if (clickbutton.matches("#charmander")) {
+//     } else if (clickbutton.matches("#charmander")) {
     
-    } else if (clickbutton.matches("#squirtle")) {
+//     } else if (clickbutton.matches("#squirtle")) {
       
-    }
-});
+//     }
+// });
